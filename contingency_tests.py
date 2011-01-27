5f9400fca41720e2e331617740ce79fa6859b3a6
Hamilton Ulmer <hamiltonulmer@Hamilton-Ulmers-MacBook-Pro-2.local>
2011-01-27 10:04:12 -0800

    Added contingency tests.

---
+++ contingency_tests.py
@@ -0,0 +1,17 @@
+import unittest
+
+class PearsonBotTests(unittest.TestCase):
+	
+	def setUp(self):
+		pass
+	
+	def tearDown(self):
+		pass
+	
+	def test_chisq_test(self):
+		pass
+	
+	def test_contingency_table(self):
+		pass
+	
+	
